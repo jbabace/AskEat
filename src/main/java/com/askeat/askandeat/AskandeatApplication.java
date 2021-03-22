6d33f22e1f5d50aba8f749f31c72e7d6dd0f16fd
2dam3 <2dam3@HZ114488.H012108.NET>
2021-03-22 12:27:09 +0100

    primer push con proyecto spring Ask&Eat

---
+++ src/main/java/com/askeat/askandeat/AskandeatApplication.java
@@ -0,0 +1,13 @@
+package com.askeat.askandeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskandeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskandeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/askeat/askandeat/AskandeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askeat.askandeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskandeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
