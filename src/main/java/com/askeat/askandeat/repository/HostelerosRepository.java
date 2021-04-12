17c37e9f5f17a901a7f9004fdf1a6b9e49f41ec7
2dam3 <ikbem@plaiaundi.net>
2021-04-12 11:41:31 +0200

    cambios de configuraciónen pom.xml y agregados repositorios y modelos de Locales y Hosteleros

---
+++ src/main/java/com/askeat/askandeat/repository/HostelerosRepository.java
@@ -0,0 +1,8 @@
+package com.askeat.askandeat.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.askeat.askandeat.models.Hosteleros;
+
+public interface HostelerosRepository extends CrudRepository<Hosteleros, Integer>{
+
+}
